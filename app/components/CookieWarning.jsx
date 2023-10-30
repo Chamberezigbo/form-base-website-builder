0e702f784d6f682cc22bb9f97e13f1f5eeb06269
chamberezigbo <chamberezigbo@gmail.com>
2023-10-30 21:24:44 +0100

    refactored the component of cookie warning

--- app/components/CookieWarning.jsx
+++ app/components/CookieWarning.jsx
@@ -6,7 +6,7 @@ const CookieWarningWrapper = styled.div`
 	color: #fff;
 	padding: 10px;
 	text-align: center;
-	display: ${(props) => (props.show ? "block" : "none")};
+	display: block; // Always set to block
 `;
 
 const CookieWarning = () => {
@@ -16,15 +16,16 @@ const CookieWarning = () => {
 		setShowWarning(false);
 	};
 
-	return (
-		<CookieWarningWrapper show={showWarning}>
+	// Use conditional rendering to render the component only when showWarning is true
+	return showWarning ? (
+		<CookieWarningWrapper>
 			This website uses cookies to ensure you get the best experience on
-			our website.{" "}
-			<button className="btn btn-primary" onClick={handleClose}>
+			our website. <br />
+			<button className="btn btn-primary mt-2" onClick={handleClose}>
 				OK
 			</button>
 		</CookieWarningWrapper>
-	);
+	) : null;
 };
 
 export default CookieWarning;
