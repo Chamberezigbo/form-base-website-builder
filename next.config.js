593a8a2b57748302c26ab0bd001a6b49f260d481
chamberezigbo <chamberezigbo@gmail.com>
2023-11-02 13:39:48 +0100

    added google verification to public file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@
 // 	images: {
 // 		unoptimized: true,
 // 	},
+// 	// Use generateStaticParams for dynamic routes
+// 	async generateStaticParams() {
+// 		return {
+// 			"/dashboard/business1": { business: "business1" },
+// 			"/dashboard/business2": { business: "business2" },
+// 			// Add more dynamic paths as needed
+// 		};
+// 	},
 // };
 
 // module.exports = nextConfig;
